88dceddc556a225eec3c7d8a758b67856240ee81
macaesp <mac.espinoza@duocuc.cl>
2026-07-13 02:15:49 -0400

    cambios

--- frontend/app.js
+++ frontend/app.js
@@ -5,8 +5,7 @@
 // Determinar la URL base de la API según el host
 // frontend/app.js
 
-const API_BASE = "http://a110e1af2ab3145fe9a6801c298334e5-2078829890.us-east-1.elb.amazonaws.com:3001/api/productos";
-
+const API_BASE = "http://a91bb2252c269471ca0488f121579142-1569537220.us-east-1.elb.amazonaws.com:3001/api/productos";
 let editandoId = null;
 
 const tbody = document.getElementById("tbodyProductos");
